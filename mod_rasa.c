c721984efcc398b64b5d029ef402cadc8de9abc8
lihaibin <lihaibin@tangbull.com>
2021-08-20 11:15:35 +0800

    update

--- mod_rasa.c
+++ mod_rasa.c
@@ -12,11 +12,9 @@ SWITCH_MODULE_DEFINITION(mod_rasa, mod_rasa_load, mod_rasa_shutdown, NULL);
 
 
 SWITCH_STANDARD_API(rasa_function){
-
+	const int billsec = 10;
 	switch_channel_t *channel = switch_core_session_get_channel(session);
-	if (switch_channel_get_state(channel) == CS_HANGUP) {
-	const char *billsec = = switch_channel_get_variable(channel, "billsec");
-	}
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "%d\n",billsec);
 	return SWITCH_STATUS_SUCCESS;
 
 }

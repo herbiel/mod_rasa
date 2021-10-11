26a0f74d2e0e408d4bbbe4e4cd507f171b7acc8b
herbiel <466039454@qq.com>
2021-10-11 22:04:19 +0800

    update

--- mod_rasa.c
+++ mod_rasa.c
@@ -6,6 +6,42 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_rasa_load);
 SWITCH_MODULE_RUNTIME_FUNCTION(mod_rasa_runtime);
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_rasa_shutdown);
 
+struct record_helper {
+	switch_media_bug_t *bug;
+	switch_memory_pool_t *helper_pool;
+	switch_core_session_t *recording_session;
+	switch_core_session_t *transfer_from_session;
+	uint8_t transfer_complete;
+	char *file;
+	switch_file_handle_t *fh;
+	switch_file_handle_t in_fh;
+	switch_file_handle_t out_fh;
+	int native;
+	uint32_t packet_len;
+	int min_sec;
+	int final_timeout_ms;
+	int initial_timeout_ms;
+	int silence_threshold;
+	int silence_timeout_ms;
+	switch_time_t silence_time;
+	int rready;
+	int wready;
+	switch_time_t last_read_time;
+	switch_time_t last_write_time;
+	switch_bool_t hangup_on_error;
+	switch_codec_implementation_t read_impl;
+	switch_bool_t speech_detected;
+	switch_buffer_t *thread_buffer;
+	switch_thread_t *thread;
+	switch_mutex_t *buffer_mutex;
+	int thread_ready;
+	uint8_t thread_needs_transfer;
+	uint32_t writes;
+	uint32_t vwrites;
+	const char *completion_cause;
+	int start_event_sent;
+	switch_event_t *variables;
+};
 
 // Actually it explains as followings:
 // static const char modname[] = "mod_rasa";
@@ -25,6 +61,7 @@ SWITCH_STANDARD_API(rasa_function){
 
 static switch_bool_t record_callback(switch_media_bug_t *bug, void *user_data, switch_abc_type_t type)
 {
+	switch_size_t len = 0;
 	switch (type) {
 	case SWITCH_ABC_TYPE_INIT: {//媒体bug设置成功
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "test -----1 \n");
@@ -40,6 +77,20 @@ static switch_bool_t record_callback(switch_media_bug_t *bug, void *user_data, s
 	}
 	case SWITCH_ABC_TYPE_READ_PING:
 	{//读取到音频流
+		switch_size_t len;
+		uint8_t data[SWITCH_RECOMMENDED_BUFFER_SIZE];
+		switch_frame_t frame = { 0 };
+		switch_status_t status;
+		int i = 0;
+		frame.data = data;
+		frame.buflen = SWITCH_RECOMMENDED_BUFFER_SIZE;
+		for (;;) {
+			status = switch_core_media_bug_read(bug, &frame, i++ == 0 ? SWITCH_FALSE : SWITCH_TRUE);
+			len = (switch_size_t) frame.datalen / 2 / frame.channels;
+			switch_core_file_write(rh->fh, mask ? null_data : data, &len)
+			rh->writes++;
+
+		}
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "test -----READ_PING \n");
 		break;
 	}
@@ -56,9 +107,18 @@ SWITCH_STANDARD_APP(rasa_session_function)
 {
 	switch_media_bug_t *bug;
 	switch_status_t status;
-
-	if ((status = switch_core_media_bug_add(session, "my_rasa", "",
-										record_callback, "", 0, SMBF_READ_STREAM | SMBF_WRITE_STREAM | SMBF_READ_PING, &bug)) != SWITCH_STATUS_SUCCESS) {
+	time_t to = 0;
+	switch_media_bug_flag_t flags = SMBF_READ_STREAM | SMBF_WRITE_STREAM | SMBF_READ_PING;
+	struct record_helper *rh = NULL;
+	char *path = NULL;
+	path = switch_core_session_strdup(session, data);
+	if ((status = record_helper_create(&rh, session)) != SWITCH_STATUS_SUCCESS) {
+		return status;
+	}
+	fh = switch_core_alloc(rh->helper_pool, sizeof(*fh)
+	rh->fh = fh;
+	if ((status = switch_core_media_bug_add(session, "my_rasa", path,
+										record_callback, rh, to, flags, &bug)) != SWITCH_STATUS_SUCCESS) {
 	switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_ERROR, "Error adding media bug for file \n");
 	}
 
@@ -85,7 +145,7 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_rasa_load) {
     *module_interface = switch_loadable_module_create_module_interface(pool, modname);
     switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "Hello World!\n");
     SWITCH_ADD_API(api_interface, "rasa", "RASA API", rasa_function, "syntax");
-	SWITCH_ADD_APP(app_interface, "start_my_rasa", "start_my_rasa", "start_my_rasa", start_rasa_function, "", SAF_NONE);
+	SWITCH_ADD_APP(app_interface, "start_my_rasa", "start_my_rasa", "start_my_rasa", start_rasa_function, "<path>", SAF_NONE);
 	SWITCH_ADD_APP(app_interface, "my_rasa", "my_rasa", "my_rasa", rasa_session_function, "<path>", SAF_NONE);
     return SWITCH_STATUS_SUCCESS;
 }
